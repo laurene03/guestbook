7bfcec9d651f9d09f8395a15391a6ed8bb46ad75
laurene03 <laurenek06@gmail.com>
2025-11-25 17:16:49 +0100

    Création d'un formulaire

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller; // App => scr
 
+use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -21,11 +25,30 @@ final class ConferenceController extends AbstractController
     }
 
     #[Route('/conference/{id}', name: 'conference')]
-    public function show(Conference $conference, CommentRepository $commentRepository): Response
-    {
+    public function show(
+        Conference $conference, 
+        CommentRepository $commentRepository, 
+        EntityManagerInterface $entityManager, 
+        Request $request
+        ): Response {
+
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        $form -> handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $comment -> setConference($conference);
+
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            return $this-> redirectToRoute('conference', ['id' => $conference->getId()]);
+        }
+
         return $this->render('conference/show.html.twig', [
             'conference' => $conference,
             'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']),
+            'comment_form' => $form
         ]);
     }
 }
---
+++ src/Form/Comment1Type.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use App\Entity\Conference;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Comment1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('author')
+            ->add('text')
+            ->add('email')
+            ->add('createdAt', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('conference', EntityType::class, [
+                'class' => Conference::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
